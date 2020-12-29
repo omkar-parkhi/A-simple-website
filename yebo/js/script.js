f923a4a917f3bed78fa76eeee0e4e7a5d6d0a968
omkar-parkhi <76740351+omkar-parkhi@users.noreply.github.com>
2020-12-29 20:31:57 +0530

    Add files via upload

---
+++ yebo/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+	autoplay:true,
+	autoplayHoverPause:true,
+	items:1,
+	dots:true,
+	loop:true,
+	lazyload:true,
+	margin:5,
+	stagePadding:5,
+	responsive:{
+		0:{
+			items:1,
+			dots:false	
+		},
+		485:{
+			items:1,
+			dots:false	
+		},
+		728:{
+			items:1,
+			loop:true
+		},
+		960:{
+			items:1,
+			loop:true	
+		},
+		1200:{
+			items:1,
+			dots:true	
+		}
+	}
+	});
+});
